b4d357ed3ab4a1257cb00c2bfd9f6621fec3998b
Udbhav Prasad <32470339+UdbhavPrasad072300@users.noreply.github.com>
2020-12-09 13:02:02 -0500

    added transformer encoder

--- models/transformer.py
+++ models/transformer.py
@@ -6,6 +6,8 @@ import math
 
 
 def attention(q, k, v, d_k, mask=None, dropout=0.2):
+    dropout_layer = nn.Dropout(dropout)
+
     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
 
     if mask is not None:
@@ -13,7 +15,7 @@ def attention(q, k, v, d_k, mask=None, dropout=0.2):
         scores = scores.masked_fill(mask == 0, -1e9)
 
     scores = F.softmax(scores, dim=-1)
-    scores = dropout(scores)
+    scores = dropout_layer(scores)
     out = torch.matmul(scores, v)
 
     return out
@@ -30,7 +32,8 @@ class PositionalEncoding(nn.Module):
 
         position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, embedding_size, 2).float() * (-math.log(10000.0) / embedding_size))
-        self.pe_matrix[:, 0::2] = torch.sin(position * div_term)  # Source: https://pytorch.org/tutorials/beginner/transformer_tutorial
+        self.pe_matrix[:, 0::2] = torch.sin(
+            position * div_term)  # Source: https://pytorch.org/tutorials/beginner/transformer_tutorial
         self.pe_matrix[:, 1::2] = torch.cos(position * div_term)
 
         self.pe_matrix = self.pe_matrix.unsqueeze(0).transpose(0, 1)
@@ -68,11 +71,38 @@ class MultiHeadAttention(nn.Module):
         k = self.K(k).reshape(batch_size, -1, self.num_heads, self.head_size).transpose(1, 2)
         v = self.V(v).reshape(batch_size, -1, self.num_heads, self.head_size).transpose(1, 2)
 
-        scores = attention(q, k, v, self.num_heads, mask)
+        scores = attention(q, k, v, self.num_heads, mask, self.dropout)
         concatenated = scores.transpose(1, 2).reshape(batch_size, -1, self.embed_size)
         out = self.linear(concatenated)
 
         return out
+
+
+class Transformer_Encoder_Layer(nn.Module):
+    def __init__(self, embed_size, num_heads, ff_hidden_size, dropout=0.2, device="cpu"):
+        super(Transformer_Encoder_Layer, self).__init__()
+
+        self.embed_size = embed_size
+        self.num_heads = num_heads
+        self.ff_hidden_size = ff_hidden_size
+        self.dropout = dropout
+        self.device = device
+
+        self.Norm1 = nn.LayerNorm(self.embed_size)
+        self.Norm2 = nn.LayerNorm(self.embed_size)
+
+        self.multi_attention = MultiHeadAttention(self.embed_size, self.num_heads, self.dropout)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(self.embed_size, self.ff_hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.ff_hidden_size, self.embed_size),
+            nn.Dropout(self.dropout),
+        )
+
+    def forward(self, x, mask=None):
+        x += self.Norm1(self.multi_attention(x, x, x, mask, self.dropout))
+        x += self.Norm2(self.feed_forward(x))
+        return x
 
 
 class Transformer(nn.Module):
